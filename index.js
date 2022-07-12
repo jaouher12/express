245eeb6e39915d22755a872cf3102218d7799d18
jaouher12 <80283674+jaouher12@users.noreply.github.com>
2022-07-12 23:00:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const app = express();
 var path = require('path');
 let date_ob = new Date();
 let hours = date_ob.getHours();
-
+let day=date_ob.getDay();
 
 app.set('view engine', 'pug');
 app.set('views','./views');
 const port=3000;
 
 const myLogger = function (req, res, next) {
-    if(hours<9||hours>23)
+    if(hours<9||hours>19||day.toString()==='Sunday'||day.toString()==='Saturday')
     {
         res.status(400).send('error')
     }
